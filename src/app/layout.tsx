ab9f6e038716a4ff7189390bd5ffe2520ccd0bfc
YuitoAkatsuki <yuito@yuito.work>
2024-12-21 21:31:57 +0900

    :sparkles: タイトルを「UniProject公式サイト」から「UniProject」に変更

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
                   height={50}
                 />
                 <div>
-                  UniProject公式サイト
+                  UniProject
                 </div>
               </h1>
             </Link>
