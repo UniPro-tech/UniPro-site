9211aba6cec685c096c3453c23966988cafba149
Ryunosuke Yoda <147054812+takoyakidath@users.noreply.github.com>
2026-03-28 12:32:09 +0900

    update redirect URL to the new signup page (#224)

--- src/app/join/page.tsx
+++ src/app/join/page.tsx
@@ -1,7 +1,7 @@
 import { redirect } from "next/navigation";
 
 const page = async () => {
-  redirect("https://forms.gle/VsRQQ4LwnxvDUyH58");
+  redirect("https://unique.uniproject.jp/signup");
   return (
     <main className="flex flex-col items-center justify-center w-full h-screen">
       <h1 className="text-xl font-sansjp">リダイレクト中...</h1>
